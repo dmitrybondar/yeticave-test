df077d837d4a9c52aee2e9ac2617533429c967bb
Dmitry Bondar <dimabondar90@gmail.com>
2017-12-03 22:02:39 +0200

    Переписал schema.sql на более свежий синтаксис

--- schema.sql
+++ schema.sql
@@ -1,49 +1,57 @@
-CREATE DATABASE yeticave;
+CREATE DATABASE `yeticave`;
 
-USE yeticave;
+USE `yeticave`;
 
-CREATE TABLE categories (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  title CHAR(64)
-);
+CREATE TABLE `categories` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(64) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE users (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  registration_date DATETIME,
-  email CHAR(64),
-  name CHAR(64),
-  password CHAR(32),
-  avatar CHAR(64),
-  contacts CHAR(128)
-);
+CREATE TABLE `users` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `registration_date` DATETIME DEFAULT NULL,
+  `email` VARCHAR(64) DEFAULT NULL,
+  `name` VARCHAR(64) DEFAULT NULL,
+  `password` VARCHAR(72) DEFAULT NULL,
+  `avatar` VARCHAR(64) DEFAULT NULL,
+  `contacts` VARCHAR(128) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE lots (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  title CHAR(64),
-  description TEXT,
-  start_date DATETIME,
-  end_date DATETIME,
-  img CHAR(64),
-  price INT,
-  min_bet INT,
-  user_id INT,
-	winner_id	INT,
-	category_id INT,
-	FOREIGN KEY (user_id) REFERENCES users(id),
-	FOREIGN KEY (winner_id) REFERENCES users(id),
-	FOREIGN KEY (category_id) REFERENCES categories(id)
-);
+CREATE TABLE `lots` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(64) DEFAULT NULL,
+  `description` TEXT DEFAULT NULL,
+  `start_date` DATETIME DEFAULT NULL,
+  `end_date` DATETIME DEFAULT NULL,
+  `img` VARCHAR(64) DEFAULT NULL,
+  `price` INT(11) DEFAULT NULL,
+  `min_bet` INT(11) DEFAULT NULL,
+  `user_id` INT(11) DEFAULT NULL,
+  `winner_id` INT(11) DEFAULT NULL,
+  `category_id` INT(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+	KEY `winner_id` (`winner_id`),
+	KEY `category_id` (`category_id`),
+	CONSTRAINT `lots_fk_1` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`),
+	CONSTRAINT `lots_fk_2` FOREIGN KEY (`winner_id`) REFERENCES `users`(`id`),
+	CONSTRAINT `lots_fk_3` FOREIGN KEY (`category_id`) REFERENCES `categories`(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE bets (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  date DATETIME,
-  cost INT,
-  user_id INT,
-  lot_id INT,
-  FOREIGN KEY (user_id) REFERENCES users(id),
-	FOREIGN KEY (lot_id) REFERENCES lots(id)
-);
+CREATE TABLE `bets` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `date` datetime DEFAULT NULL,
+  `cost` INT(11) DEFAULT NULL,
+  `user_id` INT(11) DEFAULT NULL,
+  `lot_id` INT(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `lot_id` (`lot_id`),
+  CONSTRAINT `bets_fk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `bets_fk_2` FOREIGN KEY (`lot_id`) REFERENCES `lots` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE UNIQUE INDEX email ON users(email);
-CREATE UNIQUE INDEX category ON categories(title);
-
+CREATE UNIQUE INDEX `email` ON users(email);
+CREATE UNIQUE INDEX `category` ON categories(title);
